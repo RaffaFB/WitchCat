3aedb354ac18a0a63d8200730f9eff3e0e63f302
Rafaella Busch <rafaellabusch@dcc.ufrj.br>
2019-08-12 14:21:46 +0000

    Add new file

---
+++ WitchCat/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour {
+	GameObject Player;
+	// Use this for initialization
+	void Start () {
+		Player = gameObject.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision){
+		if (collision.collider.tag == "Ground"){
+			Player.GetComponent<Mov2d>().noChao = true;
+		}
+	}
+	private void OnCollisionExit2D(Collision2D collision){
+		if (collision.collider.tag == "Ground"){
+			Player.GetComponent<Mov2d>().noChao = false;
+		}
+		
+	}
+}
