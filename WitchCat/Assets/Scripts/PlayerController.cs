18a219d46ae5076182904a95baff0ae6416872b9
Rafaella Busch <rafaellabusch@dcc.ufrj.br>
2019-08-12 13:55:13 +0000

    Update PlayerController.cs

--- WitchCat/Assets/Scripts/PlayerController.cs
+++ WitchCat/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Mov2d : MonoBehaviour {
+	public float velocidade = 5f;
+	public bool noChao = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Pulo ();
+		Vector3 movimento = new Vector3 (Input.GetAxis("Horizontal"), 0f, 0f);
+		transform.position += movimento * Time.deltaTime * velocidade;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Pulo(){
+		if (Input.GetButtonDown("Jump") && noChao == true){
+			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,5f), ForceMode2D.Impulse);
+		}
+
+	}
 }
